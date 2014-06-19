bf12175a46304b5ae2139d50f2997718efb153e2
Andre Kaminski <andre@geoscc.com>
2014-06-19 08:02:03 -0700

    Changed commment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function caches inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
